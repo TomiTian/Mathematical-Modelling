64f384ce71b31dc5f423fc002b4fcd58781fbc5f
Rolloos <timonrolleman@gmail.com>
2024-12-02 15:29:48 +0100

    Trying to get a confusion matrix

--- CNN/pytorch_example.py
+++ CNN/pytorch_example.py
@@ -6,6 +6,11 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -33,8 +38,47 @@ class Net(nn.Module):
         return output
 
 
+# def train(args, model, device, train_loader, optimizer, epoch):
+#     model.train()
+#     for batch_idx, (data, target) in enumerate(train_loader):
+#         data, target = data.to(device), target.to(device)
+#         optimizer.zero_grad()
+#         output = model(data)
+#         loss = F.nll_loss(output, target)
+#         loss.backward()
+#         optimizer.step()
+#         if batch_idx % args.log_interval == 0:
+#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+#                 epoch, batch_idx * len(data), len(train_loader.dataset),
+#                 100. * batch_idx / len(train_loader), loss.item()))
+#             if args.dry_run:
+#                 break
+
+
+# def test(model, device, test_loader):
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         for data, target in test_loader:
+#             data, target = data.to(device), target.to(device)
+#             output = model(data)
+#             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+#             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+#             correct += pred.eq(target.view_as(pred)).sum().item()
+
+#     test_loss /= len(test_loader.dataset)
+
+#     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+#         test_loss, correct, len(test_loader.dataset),
+#         100. * correct / len(test_loader.dataset)))
+
+
+
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    all_preds = []
+    all_targets = []
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -42,6 +86,12 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+
+        # Collect predictions for confusion matrix
+        preds = output.argmax(dim=1, keepdim=True).squeeze()
+        all_preds.extend(preds.cpu().numpy())
+        all_targets.extend(target.cpu().numpy())
+
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -49,18 +99,24 @@ def train(args, model, device, train_loader, optimizer, epoch):
             if args.dry_run:
                 break
 
+    return np.array(all_preds), np.array(all_targets)
+
 
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
+    all_preds = []
+    all_targets = []
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            preds = output.argmax(dim=1, keepdim=True).squeeze()
+            all_preds.extend(preds.cpu().numpy())
+            all_targets.extend(target.cpu().numpy())
+            correct += preds.eq(target.view_as(preds)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
@@ -68,32 +124,24 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+    return np.array(all_preds), np.array(all_targets)
+
+
+
+def plot_confusion_matrix(y_true, y_pred, classes, title="Confusion Matrix", save_path=None):
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
+    disp.plot(cmap=plt.cm.Blues)
+    plt.title(title)
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
+    # Add arguments (omitted for brevity, keep as is)
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     use_mps = not args.no_mps and torch.backends.mps.is_available()
@@ -116,28 +164,106 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform=transforms.Compose([
+    transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
-        ])
+    ])
     dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
+                              transform=transform)
     dataset2 = datasets.MNIST('../data', train=False,
-                       transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+                              transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+        train_preds, train_targets = train(args, model, device, train_loader, optimizer, epoch)
+        test_preds, test_targets = test(model, device, test_loader)
         scheduler.step()
+
+        # Plot confusion matrices
+        classes = [str(i) for i in range(10)]
+        plot_confusion_matrix(train_targets, train_preds, classes, title=f"Train Confusion Matrix Epoch {epoch}",
+                              save_path=f"train_confusion_matrix_epoch_{epoch}.png")
+        plot_confusion_matrix(test_targets, test_preds, classes, title=f"Test Confusion Matrix Epoch {epoch}",
+                              save_path=f"test_confusion_matrix_epoch_{epoch}.png")
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+# def main():
+#     # Training settings
+#     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+#     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+#                         help='input batch size for training (default: 64)')
+#     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+#                         help='input batch size for testing (default: 1000)')
+#     parser.add_argument('--epochs', type=int, default=14, metavar='N',
+#                         help='number of epochs to train (default: 14)')
+#     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+#                         help='learning rate (default: 1.0)')
+#     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+#                         help='Learning rate step gamma (default: 0.7)')
+#     parser.add_argument('--no-cuda', action='store_true', default=False,
+#                         help='disables CUDA training')
+#     parser.add_argument('--no-mps', action='store_true', default=False,
+#                         help='disables macOS GPU training')
+#     parser.add_argument('--dry-run', action='store_true', default=False,
+#                         help='quickly check a single pass')
+#     parser.add_argument('--seed', type=int, default=1, metavar='S',
+#                         help='random seed (default: 1)')
+#     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+#                         help='how many batches to wait before logging training status')
+#     parser.add_argument('--save-model', action='store_true', default=True,
+#                         help='For Saving the current Model')
+#     args = parser.parse_args()
+#     use_cuda = not args.no_cuda and torch.cuda.is_available()
+#     use_mps = not args.no_mps and torch.backends.mps.is_available()
+
+#     torch.manual_seed(args.seed)
+
+#     if use_cuda:
+#         device = torch.device("cuda")
+#     elif use_mps:
+#         device = torch.device("mps")
+#     else:
+#         device = torch.device("cpu")
+
+#     train_kwargs = {'batch_size': args.batch_size}
+#     test_kwargs = {'batch_size': args.test_batch_size}
+#     if use_cuda:
+#         cuda_kwargs = {'num_workers': 1,
+#                        'pin_memory': True,
+#                        'shuffle': True}
+#         train_kwargs.update(cuda_kwargs)
+#         test_kwargs.update(cuda_kwargs)
+
+#     transform=transforms.Compose([
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.1307,), (0.3081,))
+#         ])
+#     dataset1 = datasets.MNIST('../data', train=True, download=True,
+#                        transform=transform)
+#     dataset2 = datasets.MNIST('../data', train=False,
+#                        transform=transform)
+#     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+#     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+#     model = Net().to(device)
+#     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+#     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+#     for epoch in range(1, args.epochs + 1):
+#         train(args, model, device, train_loader, optimizer, epoch)
+#         test(model, device, test_loader)
+#         scheduler.step()
+
+#     if args.save_model:
+#         torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == '__main__':
