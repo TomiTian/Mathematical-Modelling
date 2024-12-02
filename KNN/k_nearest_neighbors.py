cdb3a3584bbb7985cb8e66ab318ded2fcaf38336
Vertic03 <132469373+Vertic03@users.noreply.github.com>
2024-12-02 14:22:43 +0100

    Added quantiled sampling, quantiled averages

--- KNN/k_nearest_neighbors.py
+++ KNN/k_nearest_neighbors.py
@@ -3,7 +3,7 @@ import struct
 import matplotlib.pyplot as plt
 import vptree
 import random
-from statistics import mode
+from statistics import mode, multimode
 
 # Load images from the MNIST dataset
 def load_image_file(filename):
@@ -86,13 +86,56 @@ def n_random_images_each(training_img_set, training_label_set, n):
         random_images_labels.extend([i] * n)
     return random_images, random_images_labels
 
+def n_quantiled_images_each(training_img_set, training_label_set, n):
+    quantiled_images = []
+    quantiled_images_labels = []
+    averages = average_images(training_img_set, training_label_set)
+    for i in range(10):
+        digit_images = [training_img_set[j] for j in range(len(training_img_set)) if training_label_set[j] == i]
+        num_images = len(digit_images)
+        distances_from_avg = [euclid_dist(img, averages[i]) for img in digit_images]
+        sorted_indices = np.argsort(distances_from_avg)
+        sorted_digit_images = [digit_images[j] for j in sorted_indices]
+        quantiled_digit_images = [sorted_digit_images[num_images * j // n] for j in range(n)]
+        quantiled_images.extend(quantiled_digit_images)
+        quantiled_images_labels.extend([i] * n)
+    return quantiled_images, quantiled_images_labels
+
+def n_quantiled_averages_each(training_img_set, training_label_set, n):
+    quantiled_averages = []
+    quantiled_averages_labels = []
+    averages = average_images(training_img_set, training_label_set)
+    for i in range(10):
+        digit_images = [training_img_set[j] for j in range(len(training_img_set)) if training_label_set[j] == i]
+        num_images = len(digit_images)
+        distances_from_avg = [euclid_dist(img, averages[i]) for img in digit_images]
+        sorted_indices = np.argsort(distances_from_avg)
+        sorted_digit_images = [digit_images[j] for j in sorted_indices]
+        quantiled_digit_averages = []
+        for j in range(n):
+            quantiled_subset = sorted_digit_images[(num_images * j // n) : (num_images * (j+1) // n)]
+            sum_images = [0] * 784
+            for k in range(len(quantiled_subset)):
+                for l in range(784):
+                    sum_images[l] += quantiled_subset[k][l]
+            avg_digit = [pixel/len(quantiled_subset) for pixel in sum_images]
+            quantiled_digit_averages.append(avg_digit)
+        quantiled_averages.extend(quantiled_digit_averages)
+        quantiled_averages_labels.extend([i] * n)
+    return quantiled_averages, quantiled_averages_labels
+
 def k_nearest_neighbors_old(training_img_set, training_label_set, img, k):
     distances_list = [euclid_dist(i, img) for i in training_img_set]
     distances_array = np.array(distances_list)
     neighbor_indices = np.argpartition(distances_array, k-1)[0:k].tolist()
     neighbors = [training_label_set[i] for i in neighbor_indices]
-    #print(neighbors)
-    digit = mode(neighbors)
+    print(neighbors)
+    digits = multimode(neighbors)
+    if (len(digits) == 1):
+        digit = digits[0]
+    else:
+        closest_index = distances_list.index(min(distances_list))
+        digit = training_label_set[closest_index]
     return digit
 
 def k_nearest_neighbors(tree: vptree, img, k):
@@ -112,7 +155,7 @@ train, test = load_mnist()
 #guess = k_nearest_neighbors(train['x'], train['y'], test['x'][1], 3)
 #print(guess)
 #averages = average_images(train['x'], train['y'])
-random_10_of_each, random_10_labels = n_random_images_each(train['x'], train['y'], 10)
+"""random_10_of_each, random_10_labels = n_random_images_each(train['x'], train['y'], 10)
 correctness_list = []
 for i in range(0, 400):
     guess = k_nearest_neighbors_old(random_10_of_each, random_10_labels, test['x'][i], 1)
@@ -122,7 +165,7 @@ for i in range(0, 400):
     else: correctness_list.append(0)
 
 accuracy = sum(correctness_list) / len(correctness_list)
-print("Accuracy is " + str(accuracy))
+print("Accuracy is " + str(accuracy))"""
 
 """train_augmented = augment_images(train['x'][0:5000], train['y'][0:5000])
 print("augmented yay")
@@ -132,3 +175,36 @@ for i in range(0, 20):
     imagee = test['x'][i].tolist()
     guess = k_nearest_neighbors(tree, imagee, 3)
     print(guess)"""
+
+quantiled_averages, quantiled_averages_labels = n_quantiled_images_each(train['x'], train['y'], 10)
+correctness_list = []
+for i in range(1000):
+    guess = k_nearest_neighbors_old(quantiled_averages, quantiled_averages_labels, test['x'][i], 3)
+    #print(guess)
+    if (guess == test['y'][i]):
+        correctness_list.append(1)
+    else: correctness_list.append(0)
+
+accuracy = sum(correctness_list) / len(correctness_list)
+print("Accuracy is " + str(accuracy))
+
+"""quantiled_images, quantiled_images_labels = n_quantiled_images_each(train['x'], train['y'], 10)
+averages.extend(quantiled_images)
+labels = [i for i in range(10)]
+labels.extend(quantiled_images_labels)
+correctness_list = []
+for i in range(1000):
+    guess = k_nearest_neighbors_old(averages, labels, test['x'][i], 3)
+    #print(guess)
+    if (guess == test['y'][i]):
+        correctness_list.append(1)
+    else: correctness_list.append(0)
+
+accuracy = sum(correctness_list) / len(correctness_list)
+print("Accuracy is " + str(accuracy))"""
+
+"""
+for i in range(10):
+    img_arr = np.array(quantiled_digit_images[i])
+    img = np.reshape(img_arr, 28*28)
+    show_digit(img)"""
